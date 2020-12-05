b841f3e5ef83fba229d9211de3d13f46a3153266
Julius Schmidt <newceptiondev@googlemail.com>
2020-12-05 11:51:20 +0100

    Added Solution for Day 5

---
+++ src/day5/Day5.java
@@ -0,0 +1,146 @@
+package day5;
+
+import util.FileUtil;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+        new Day5();
+    }
+
+    public Day5() {
+        List<String> inputs = FileUtil.readFileAsListOfLines("day5Task1Input");
+
+        System.out.println("Task 1: " + task1(inputs));
+        System.out.println("Task 2: " + task2(inputs));
+    }
+
+    /**
+     * Task 1
+     *
+     * @param inputs List of String
+     * @return Highest Seat Id
+     */
+    public int task1(final List<String> inputs) {
+        List<Seat> takenSeats = parseInputsToSeats(inputs);
+
+        int highestSeatId = 0;
+
+        for (Seat seat : takenSeats){
+            int seatId = seat.calculateSeatId();
+
+            if(seatId > highestSeatId){
+                highestSeatId = seatId;
+            }
+        }
+
+        return highestSeatId;
+    }
+
+    /**
+     * Task 2
+     *
+     * @param inputs List of String
+     * @return The SeatId of the searched Seat
+     */
+    public int task2(final List<String> inputs){
+        List<Seat> takenSeats = parseInputsToSeats(inputs);
+
+        Set<Integer> idsFromTakenSeats = calculateSeatIdsForAllSeats(takenSeats);
+        Map<Integer, Set<Seat>> seatsPerRow = sortSeatsToRows(takenSeats);
+
+        Set<Seat> missingSeats = new HashSet<>();
+
+        for(Integer row : seatsPerRow.keySet()){
+            if(seatsPerRow.get(row).size() != 8){
+                missingSeats.addAll(createMissingSeats(seatsPerRow.get(row)));
+            }
+        }
+
+        for(Seat seat: missingSeats){
+            int seatId = seat.calculateSeatId();
+
+            //Checks for a SeatId where the SeatIds +1 and -1 are present
+            if(idsFromTakenSeats.contains(seatId - 1) && idsFromTakenSeats.contains(seatId + 1)){
+                return seatId;
+            }
+        }
+
+        System.err.println("There was no Id that matches the Requirements");
+        return -1;
+    }
+
+    /**
+     * Parses the Input to Seats
+     *
+     * @param inputs List of String
+     * @return List of Seat
+     */
+    private List<Seat> parseInputsToSeats(final List<String> inputs) {
+        return inputs.stream().map(Seat::new).collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a Set with the SeatIds for all taken Seats
+     *
+     * @param takenSeats List of Seat
+     * @return Set of Integer
+     */
+    private Set<Integer> calculateSeatIdsForAllSeats(final List<Seat> takenSeats){
+        return takenSeats.stream().map(Seat::calculateSeatId).collect(Collectors.toSet());
+    }
+
+    /**
+     * Sorts the Seats by their Row
+     *
+     * @param takenSeats List of Seat
+     * @return Map of Integer and Set of Seat
+     */
+    private Map<Integer, Set<Seat>> sortSeatsToRows(final List<Seat> takenSeats){
+        Map<Integer, Set<Seat>> seatsByRow = new HashMap<>();
+
+        for(Seat seat : takenSeats){
+            if(seatsByRow.containsKey(seat.getRow())){
+                seatsByRow.get(seat.getRow()).add(seat);
+            } else {
+                Set<Seat> seatRow = new HashSet<>();
+                seatRow.add(seat);
+
+                seatsByRow.put(seat.getRow(), seatRow);
+            }
+        }
+
+        return seatsByRow;
+    }
+
+    /**
+     * Creates all missing Seats from the Row
+     *
+     * @param takenSeats Set with all taken Seats in this Row
+     * @return Set with the missing Seats from this Row
+     */
+    private Set<Seat> createMissingSeats(final Set<Seat> takenSeats) {
+        Set<Integer> fullColumn = IntStream.rangeClosed(0, 7).boxed().collect(Collectors.toSet());
+        int row = -1;
+
+        for(Seat seat : takenSeats){
+            if(row == -1){
+                row = seat.getRow();
+            }
+
+            fullColumn.remove(seat.getColumn());
+        }
+
+        Set<Seat> missingSeats = new HashSet<>();
+
+        for(Integer missingColumn : fullColumn){
+            missingSeats.add(new Seat(row, missingColumn));
+        }
+
+        return missingSeats;
+    }
+}
---
+++ src/day5/Seat.java
@@ -0,0 +1,145 @@
+package day5;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Seat {
+
+    public static final int COLUMN_LOWER_END = 0;
+    public static final int COLUMN_UPPER_END = 7;
+    public static final char COLUMN_LOWER_INDICATOR = 'L';
+    public static final int ROW_LOWER_END = 0;
+    public static final int ROW_UPPER_END = 127;
+    public static final char ROW_LOWER_INDICATOR = 'F';
+    private int row;
+    private int column;
+
+    public Seat(final String code) {
+        this.createSeatFromCode(code);
+    }
+
+    public Seat(final int row, final int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * Calculates the Seat Id
+     *
+     * @return SeatId
+     */
+    public int calculateSeatId() {
+        return (this.row * 8) + column;
+    }
+
+    /**
+     * Parses the Row and Column from a given Code
+     *
+     * @param code The Code that contains Row and Column
+     */
+    private void createSeatFromCode(final String code) {
+        String rowCode = code.substring(0, 7);
+        String columnCode = code.substring(7);
+
+        this.row = this.parseRow(rowCode);
+        this.column = this.parseColumn(columnCode);
+    }
+
+    /**
+     * Parses the Column
+     *
+     * @param columnCode Part of Code that describes the Column
+     * @return Column
+     */
+    private int parseColumn(final String columnCode) {
+        List<Character> columnCodeAsCharList = parseStringToCharList(columnCode);
+
+        return findCorrectPartition(columnCodeAsCharList, COLUMN_LOWER_END, COLUMN_UPPER_END,
+                COLUMN_LOWER_INDICATOR);
+    }
+
+    /**
+     * Parses the Row
+     *
+     * @param rowCode Part of Code that describes the Row
+     * @return Row
+     */
+    private int parseRow(final String rowCode) {
+        List<Character> rowCodeAsCharacter = parseStringToCharList(rowCode);
+
+        return findCorrectPartition(rowCodeAsCharacter, ROW_LOWER_END, ROW_UPPER_END,
+                ROW_LOWER_INDICATOR);
+    }
+
+    /**
+     * Find the relevant Partition by the given Indicator
+     * Gets called recursively
+     *
+     * @param indicators List of Indicators
+     * @param lowerEnd Lower End of Partition
+     * @param upperEnd Upper End of Partition
+     * @param lowerIndicator Indicator to take the lower Half of the Partition
+     * @return Partition of Size 1 that is described by the Indicators
+     */
+    private int findCorrectPartition(final List<Character> indicators, final int lowerEnd, final int upperEnd, final Character lowerIndicator) {
+
+        //Exit Condition
+        if (indicators.size() == 1) {
+            if (indicators.get(0) == lowerIndicator) {
+                return lowerEnd;
+            }
+            return upperEnd;
+        }
+
+        int shiftCount = getShiftCount(lowerEnd, upperEnd);
+        List<Character> newIndicators = indicators.subList(1, indicators.size());
+
+        if (indicators.get(0) == lowerIndicator) {
+            return this.findCorrectPartition(newIndicators, lowerEnd, upperEnd - shiftCount,
+                    lowerIndicator);
+        }
+        return this.findCorrectPartition(newIndicators, lowerEnd + shiftCount, upperEnd,
+                lowerIndicator);
+    }
+
+    /**
+     * Calculates by how much the Partition shrinks
+     *
+     * @param lowerEnd Lower End of Partition
+     * @param upperEnd Upper End of Partition
+     * @return Shrink Count of Partition
+     */
+    private int getShiftCount(final int lowerEnd, final int upperEnd) {
+        return (upperEnd - lowerEnd + 1) / 2;
+    }
+
+    /**
+     * Parses a String to a List of Character
+     *
+     * @param input String
+     * @return List of Character
+     */
+    private List<Character> parseStringToCharList(final String input) {
+        return input.chars().mapToObj(
+                character -> (char) character).collect(
+                Collectors.toList());
+    }
+
+    /**
+     * Getter for Row
+     *
+     * @return Row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Getter for Column
+     *
+     * @return Column
+     */
+    public int getColumn() {
+        return column;
+    }
+}
