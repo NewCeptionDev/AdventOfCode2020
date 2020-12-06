6d51d3a17bf4fbff70c17a1232e51f9c34a1aeb8
Julius Schmidt <newceptiondev@googlemail.com>
2020-12-06 12:59:16 +0100

    Added Solution for Day 6

---
+++ src/day6/Day6.java
@@ -0,0 +1,71 @@
+package day6;
+
+import util.FileUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        new Day6();
+    }
+
+    public Day6() {
+        List<String> inputs = FileUtil.readFileAsListOfLines("day6Task1Input");
+
+        System.out.println("Task 1: " + task1(inputs));
+        System.out.println("Task 2: " + task2(inputs));
+    }
+
+    /**
+     * Task 1
+     *
+     * @param inputs List of String
+     * @return Sum of Answers per Group that were answered Yes by at least one
+     */
+    public int task1(final List<String> inputs) {
+        List<Group> groups = mapInputToGroups(inputs);
+
+        return groups.stream().map(Group::getUnionOfAnswers).map(Set::size).reduce(0, Integer::sum);
+    }
+
+    /**
+     * Task 2
+     *
+     * @param inputs List of String
+     * @return Sum of Answers per Group that were answered Yes by every Group Member
+     */
+    public int task2(final List<String> inputs){
+        List<Group> groups = mapInputToGroups(inputs);
+
+        return groups.stream().map(Group::getIntersectionOfAnswers).map(Set::size).reduce(0, Integer::sum);
+    }
+
+    /**
+     * Maps the Input to Groups
+     *
+     * @param inputs List of String
+     * @return List of Group
+     */
+    private List<Group> mapInputToGroups(final List<String> inputs) {
+        List<Group> groups = new ArrayList<>();
+
+        List<String> currentGroup = new ArrayList<>();
+
+        for (String line : inputs) {
+            if(line.length() > 0){
+                currentGroup.add(line);
+            } else {
+                //Adds the Group and resets the currentGroup
+                groups.add(new Group(currentGroup));
+                currentGroup = new ArrayList<>();
+            }
+        }
+        //Add last Group
+        groups.add(new Group(currentGroup));
+
+        return groups;
+    }
+}
---
+++ src/day6/Group.java
@@ -0,0 +1,64 @@
+package day6;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Group {
+
+    private final List<String> answers;
+
+    public Group(final List<String> answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * Returns the Unions of all Answers
+     *
+     * @return Set of Character
+     */
+    public Set<Character> getUnionOfAnswers() {
+        Set<Character> union = new HashSet<>();
+
+        for (String answer : answers) {
+            union.addAll(mapStringToCharSet(answer));
+        }
+
+        return union;
+    }
+
+    /**
+     * Returns the Intersection of all Answers
+     *
+     * @return Set of Character
+     */
+    public Set<Character> getIntersectionOfAnswers() {
+        Set<Character> intersection = null;
+
+        for (String answer : answers) {
+            if (intersection == null) {
+                intersection = mapStringToCharSet(answer);
+            } else {
+                intersection.retainAll(mapStringToCharSet(answer));
+            }
+        }
+
+        return intersection;
+    }
+
+    /**
+     * Maps a String to a Set of Character
+     *
+     * @param input String
+     * @return Set of Character
+     */
+    private Set<Character> mapStringToCharSet(final String input) {
+        Set<Character> characterSet = new HashSet<>();
+
+        for (char character : input.toCharArray()) {
+            characterSet.add(character);
+        }
+
+        return characterSet;
+    }
+}
