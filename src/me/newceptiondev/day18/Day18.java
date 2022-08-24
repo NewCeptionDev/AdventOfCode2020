ea4d83e6e9976a715f48d875ab998e2e27d0092a
Julius Schmidt <newceptiondev@googlemail.com>
2022-08-24 17:04:28 +0200

    Finished Day18 Task 2

--- src/me/newceptiondev/day18/Day18.java
+++ src/me/newceptiondev/day18/Day18.java
@@ -2,112 +2,170 @@ package me.newceptiondev.day18;
 
 import me.newceptiondev.util.FileUtil;
 
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Day18 {
 
-  private static final String FILE_NAME = "day18Task1Input";
+    private static final String FILE_NAME = "day18Task1Input";
 
-  public static void main(String[] args) {
-    List<String> inputs = FileUtil.readFileAsListOfLines(FILE_NAME);
+    public static void main(String[] args) {
+        List<String> inputs = FileUtil.readFileAsListOfLines(FILE_NAME);
 
-    new Day18(inputs);
-  }
+        new Day18(inputs);
+    }
 
-  public Day18() {
-  }
+    public Day18() {
+    }
 
-  private Day18(List<String> inputs) {
-    System.out.println("Task 1: " + task1(inputs));
-    System.out.println("Task 2: " + task2(inputs));
-  }
+    private Day18(List<String> inputs) {
+        System.out.println("Task 1: " + task1(inputs));
+        System.out.println("Task 2: " + task2(inputs));
+    }
 
-  /**
-   * Task 1
-   *
-   * @param inputs List of String
-   *
-   * @return Evaluation Result of Expression
-   */
-  public long task1(List<String> inputs) {
+    /**
+     * Task 1
+     *
+     * @param inputs List of String
+     *
+     * @return Evaluation Result of Expression
+     */
+    public long task1(List<String> inputs) {
 
-    return inputs.stream().map(this::evaluate).reduce(Long::sum).get();
-  }
+        return inputs.stream().map(this::evaluate).reduce(Long::sum).get();
+    }
 
-  /**
-   * Task 2
-   *
-   * @param inputs List of String
-   *
-   * @return
-   */
-  public int task2(List<String> inputs) {
-    return 0;
-  }
+    /**
+     * Task 2
+     *
+     * @param inputs List of String
+     *
+     * @return
+     */
+    public BigInteger task2(List<String> inputs) {
+        return inputs.stream().map(this::reduceLine).reduce(BigInteger::add).get();
+    }
 
-  private long evaluate(String code) {
+    private long evaluate(String code) {
 
-    Operation currentOperation = null;
-    Long accumulator = null;
-    Long summand = null;
+        Operation currentOperation = null;
+        Long accumulator = null;
+        Long summand = null;
 
-    while(code.length() > 0) {
-      if(code.charAt(0) == '(') {
-        long result = evaluate(code.substring(1));
+        while(code.length() > 0) {
+            if(code.charAt(0) == '(') {
+                long result = evaluate(code.substring(1));
 
-        code = code.substring(findCorrespondingParenthesesIndex(code));
+                code = code.substring(findCorrespondingParenthesesIndex(code));
 
-        if(accumulator != null) {
-          summand = result;
-        } else {
-          accumulator = result;
+                if(accumulator != null) {
+                    summand = result;
+                } else {
+                    accumulator = result;
+                }
+            } else if(code.charAt(0) == ' ') {
+                //DO Nothing
+            } else if(code.charAt(0) == '+') {
+                currentOperation = Operation.ADDITION;
+            } else if(code.charAt(0) == '*') {
+                currentOperation = Operation.MULTIPLICATION;
+            } else if(code.charAt(0) == ')') {
+                return accumulator;
+            } else {
+                long result = Integer.parseInt(code.charAt(0) + "");
+
+                if(accumulator != null) {
+                    summand = result;
+                } else {
+                    accumulator = result;
+                }
+            }
+
+            if(accumulator != null && summand != null) {
+                if(currentOperation == Operation.MULTIPLICATION) {
+                    accumulator *= summand;
+                } else {
+                    accumulator += summand;
+                }
+                summand = null;
+            }
+            code = code.substring(1);
         }
-      } else if(code.charAt(0) == ' ') {
-        //DO Nothing
-      } else if(code.charAt(0) == '+') {
-        currentOperation = Operation.ADDITION;
-      } else if(code.charAt(0) == '*') {
-        currentOperation = Operation.MULTIPLICATION;
-      } else if(code.charAt(0) == ')') {
+
         return accumulator;
-      } else {
-        long result = Integer.parseInt(code.charAt(0) + "");
-
-        if(accumulator != null) {
-          summand = result;
-        } else {
-          accumulator = result;
-        }
-      }
-
-      if(accumulator != null && summand != null) {
-        if(currentOperation == Operation.MULTIPLICATION) {
-          accumulator *= summand;
-        } else {
-          accumulator += summand;
-        }
-        summand = null;
-      }
-      code = code.substring(1);
     }
 
-    return accumulator;
-  }
+    private int findCorrespondingParenthesesIndex(String input) {
+        int currentLevel = 1;
 
-  private int findCorrespondingParenthesesIndex(String input) {
-    int currentLevel = 1;
+        int currentIndex = 0;
+        while(currentLevel > 0 && input.length() > currentIndex) {
+            currentIndex++;
 
-    int currentIndex = 0;
-    while(currentLevel > 0 && input.length() > currentIndex) {
-      currentIndex++;
+            if(input.charAt(currentIndex) == '(') {
+                currentLevel++;
+            } else if(input.charAt(currentIndex) == ')') {
+                currentLevel--;
+            }
+        }
 
-      if(input.charAt(currentIndex) == '(') {
-        currentLevel++;
-      } else if(input.charAt(currentIndex) == ')') {
-        currentLevel--;
-      }
+        return currentIndex;
     }
 
-    return currentIndex;
-  }
+    private BigInteger reduceLine(String input) {
+        String editString = input;
+        Deque<Integer> openParenthesisPosition = new ArrayDeque<>();
+
+        for(int i = 0; i < editString.length(); i++) {
+            if(editString.charAt(i) == '(') {
+                openParenthesisPosition.push(i);
+            } else if(editString.charAt(i) == ')') {
+                int correspondingOpenParenthesis = openParenthesisPosition.pop();
+                editString = editString.substring(0, correspondingOpenParenthesis) + calculateResult(
+                    editString.substring(correspondingOpenParenthesis + 1, i)) + editString.substring(i + 1);
+                i = correspondingOpenParenthesis - 1;
+            }
+        }
+
+        return calculateResult(editString);
+    }
+
+    private BigInteger calculateResult(String input) {
+        Deque<BigInteger> numbers = new ArrayDeque<>();
+
+        boolean additionAfterNextNumber = false;
+        StringBuilder currentNumber = new StringBuilder();
+        for(int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if(Character.isDigit(currentChar)) {
+                currentNumber.append(currentChar);
+            } else if(currentChar == ' ') {
+                if(!currentNumber.isEmpty()) {
+                    BigInteger parsed = new BigInteger(currentNumber.toString());
+
+                    if(additionAfterNextNumber) {
+                        BigInteger first = numbers.pop();
+                        numbers.push(first.add(parsed));
+                        additionAfterNextNumber = false;
+                    } else {
+                        numbers.push(parsed);
+                    }
+                    currentNumber = new StringBuilder();
+                }
+            } else if(currentChar == '+') {
+                additionAfterNextNumber = true;
+            }
+        }
+
+        BigInteger parsed = new BigInteger(currentNumber.toString());
+
+        if(additionAfterNextNumber) {
+            BigInteger first = numbers.poll();
+            numbers.push(first.add(parsed));
+        } else {
+            numbers.push(parsed);
+        }
+
+        return numbers.stream().reduce(BigInteger.valueOf(1L), BigInteger::multiply);
+    }
 }
--- src/test/day18/Day18Tests.java
+++ src/test/day18/Day18Tests.java
@@ -3,6 +3,7 @@ package test.day18;
 import me.newceptiondev.day18.Day18;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,5 +19,25 @@ public class Day18Tests {
         Day18 day = new Day18();
 
         assertEquals(26, day.task1(inputs));
+    }
+
+    @Test
+    public void task2Example1Test(){
+
+        List<String> inputs = List.of("1 + 2 * 3 + 4 * 5 + 6");
+
+        Day18 day = new Day18();
+
+        assertEquals(BigInteger.valueOf(231), day.task2(inputs));
+    }
+
+    @Test
+    public void task2Example2Test(){
+
+        List<String> inputs = List.of("1 + (2 * 3) + (4 * (5 + 6))");
+
+        Day18 day = new Day18();
+
+        assertEquals(BigInteger.valueOf(51), day.task2(inputs));
     }
 }
